3b8b8945676b9892c79cf2ded8690521b0665ef6
Vladimír Mrkáček <vlad.mrkacek@gmail.com>
2017-06-04 23:55:55 +0200

    Basic Structure

---
+++ ESBackupAndReplication/AppData/Initializer.cs
@@ -0,0 +1,98 @@
+﻿using ESBackupAndReplication.AppData.Authentication;
+using ESBackupAndReplication.AppData.CRON;
+using ESBackupAndReplication.AppData.Interfaces;
+using ESBackupAndReplication.ESBackupServerService;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESBackupAndReplication.AppData
+{
+    public class Initializer: IStartable, IJob
+    {
+        #region Local properties
+        private CronScheduler _Scheduler { get; set; }
+        private User _User { get; set; }
+        private LoginResponse _LoginResponse { get; set; }
+        private Configuration _Config { get; set; }
+        #endregion       
+        public Initializer()
+        {
+            this.Start();
+        }
+
+        public void Start()
+        {
+            this._Scheduler = CronScheduler.GetInstance();
+            //TODO: Schedule clients refresh
+        }
+        public void Stop()
+        {
+            if (this._LoginResponse.UTCExpiration > DateTime.UtcNow)
+            {
+                ESBackupServerServiceClient client = new ESBackupServerServiceClient();
+                client.Logout(this._LoginResponse.SessionID);
+                client.Close();
+            }
+        }
+
+        public bool EstabilishConnection()
+        {
+            bool estabilished;
+            ESBackupServerServiceClient client = new ESBackupServerServiceClient();
+            //TODO: Correct parameters in Registration
+            RegistrationResponse response = client.RequestRegistration("PC-Name", "hwid");
+            if (response.Status == ClientStatus.Verified)
+            {
+                this._User = new User()
+                {
+                    Username = response.Username,
+                    Password = response.Password
+                };
+                estabilished = true;
+            }
+            else
+                estabilished = false;
+
+            client.Close();
+            return estabilished;
+        }
+        public void RefreshConnection()
+        {
+            ESBackupServerServiceClient client = new ESBackupServerServiceClient();
+            this._LoginResponse = client.Login(this._User.Username, this._User.Password);
+            client.ClientReportUpdated(this._LoginResponse.SessionID);
+
+            //TODO: Implement - If config has changed then download new one               
+            if (true)
+            {
+                this._Scheduler.ClearJobs();
+                this._Config = client.GetConfiguration(this._LoginResponse.SessionID);
+
+                if (this._Config.Templates.Length != 0)
+                {
+                    foreach (BackupTemplate template in this._Config.Templates)
+                    {
+                        if (template.Enabled)
+                        {                            
+                            this._Scheduler.ScheduleTemplate(template, this._LoginResponse.SessionID);
+                        }                        
+                    }
+                }
+            }
+            client.Close();
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            if (this._User == null)
+                if (this.EstabilishConnection())
+                    this.RefreshConnection();
+            else
+                this.RefreshConnection();
+        }
+    }
+}
---
+++ ESBackupAndReplication/AppData/Interfaces/IRunnable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESBackupAndReplication.AppData.Interfaces
+{
+    interface IStartable
+    {
+        void Start();
+        void Stop();
+    }
+}
